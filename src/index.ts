7045d45d952b76ed37bd5d54c02e7f2aa5480d56
王金强 <wangjinqiang@gongchang.com>
2021-11-18 12:39:20 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import * as path from "path";
+const version = "4.2.1";
+
+export const linux32 = {
+  path: path.resolve(__dirname, "../assets/ffprobe-4.2.1-linux-32"),
+  version,
+};
+
+export const linux64 = {
+  path: path.resolve(__dirname, "../assets/ffprobe-4.2.1-linux-64"),
+  version,
+};
---
+++ test/test.ts
@@ -0,0 +1,4 @@
+import { linux32, linux64 } from "../src/index";
+
+console.log(linux32);
+console.log(linux64);
